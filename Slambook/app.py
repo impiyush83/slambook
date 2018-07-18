c9f806332c653f1a07787a0f8ebd87a8e90a69f6
impiyush83 <nalawadepiyush@gmail.com>
2018-07-18 10:58:00 +0530

    CSS changed

--- Slambook/app.py
+++ Slambook/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 db = SQLAlchemy('sqlite:///slambook.db')
 
+
 class Parent(db.Model):
     __tablename__ = "parent"
 
@@ -17,6 +18,7 @@ class Parent(db.Model):
     password = db.Column(String)
     gender = db.Column(String)
 
+
 db.create_all()
 
 
@@ -25,19 +27,18 @@ def index():
     return render_template('login_and_sign_up.html')
 
 
-
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     try:
         email = request.form['email']
         password = request.form['password']
         objects = db.query(Parent).filter().all()
-        flag=0
-        for i in objects  :
-             if (str(i.email).strip()== str(email).strip() and str(i.password).strip()== str(password).strip()):
-                    flag=1
-                    break
-        if flag==1:
+        flag = 0
+        for i in objects:
+            if str(i.email).strip() == str(email).strip() and str(i.password).strip() == str(password).strip():
+                flag = 1
+                break
+        if flag == 1:
             print "1"
             return redirect(url_for('logged_in'))
         else:
@@ -53,7 +54,6 @@ def logged_in():
     return render_template('login_and_sign_up.html')
 
 
-
 @app.route('/insertuser', methods=['POST', 'GET'])
 def insertuser():
     fn = request.form['fn']
@@ -62,7 +62,7 @@ def insertuser():
     password = request.form['pass']
     gender = request.form['gender']
     print gender
-    u1 = Parent(fn=fn,sn=sn,email=email,password=password,gender=gender)
+    u1 = Parent(fn=fn, sn=sn, email=email, password=password, gender=gender)
     db.add(u1)
     db.commit()
     return redirect(url_for('index'))
