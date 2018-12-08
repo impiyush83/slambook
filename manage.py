d94d935c0349170b5493af192fec3fa20104005c
nalawadepiyush <nalawadepiyush@gmail.com>
2018-12-08 22:15:14 +0530

    Changes for heroku

--- manage.py
+++ manage.py
@@ -9,16 +9,16 @@ from slambook_restful.app import create_app
 from slambook_restful.models import db
 from slambook_restful.settings import ProdConfig, StageConfig, DevConfig, TestConfig
 
-env = os.environ.get("SLAMBOOK_ENV").lower()
-if env == 'prod':
-    app = create_app(ProdConfig)
-elif env == 'stage':
-    app = create_app(StageConfig)
-elif env == 'dev':
-    app = create_app(DevConfig)
-else:
-    app = create_app(TestConfig)
-
+# env = os.environ.get("SLAMBOOK_ENV").lower()
+# if env == 'prod':
+#     app = create_app(ProdConfig)
+# elif env == 'stage':
+#     app = create_app(StageConfig)
+# elif env == 'dev':
+#     app = create_app(DevConfig)
+# else:
+#     app = create_app(TestConfig)
+app = create_app(DevConfig)
 # migrate = Migrate(app=app, db=db, compare_type=True)
 manager = Manager(app=app)
 manager.add_command('server', Server(threaded=True))
--- run_script.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export SLAMBOOK_DEV=dev
-python manage.py db upgrade
-python manage.py server
