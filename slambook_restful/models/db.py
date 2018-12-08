cbf16b6a09b4cdfd86e4072c437b1be76288f8e4
nalawadepiyush <nalawadepiyush@gmail.com>
2018-12-09 00:08:47 +0530

    Fully working on localhost

--- slambook_restful/models/db.py
+++ slambook_restful/models/db.py
@@ -3,10 +3,8 @@ import os
 from flask import Config
 from sqlalchemy_wrapper import SQLAlchemy
 
-from manage import app
-
 config_name = 'slambook_restful.settings.{}Config'.format(os.environ.get('SLAMBOOK_ENV'))
 config = Config("")
 config.from_object(config_name)
 
-db = SQLAlchemy(uri=app.config['DATABASE_URL'])
+db = SQLAlchemy(uri=config['DATABASE_URL'])
