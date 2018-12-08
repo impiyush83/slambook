dbb986a7c354bbc212a2fefd1e3707096066ee6a
nalawadepiyush <nalawadepiyush@gmail.com>
2018-12-08 22:54:40 +0530

    Changes for local

--- manage.py
+++ manage.py
@@ -1,19 +1,21 @@
+import os
+
 from flask_migrate import MigrateCommand
 from flask_script import Manager, Server
 
 from slambook_restful.app import create_app
-from slambook_restful.settings import DevConfig
+from slambook_restful.settings import DevConfig, ProdConfig, StageConfig, TestConfig
 
-# env = os.environ.get("SLAMBOOK_ENV").lower()
-# if env == 'prod':
-#     app = create_app(ProdConfig)
-# elif env == 'stage':
-#     app = create_app(StageConfig)
-# elif env == 'dev':
-#     app = create_app(DevConfig)
-# else:
-#     app = create_app(TestConfig)
-app = create_app(DevConfig)
+env = os.environ.get("SLAMBOOK_ENV").lower()
+if env == 'prod':
+    app = create_app(ProdConfig)
+elif env == 'stage':
+    app = create_app(StageConfig)
+elif env == 'dev':
+    app = create_app(DevConfig)
+else:
+    app = create_app(TestConfig)
+
 # migrate = Migrate(app=app, db=db, compare_type=True)
 manager = Manager(app=app)
 manager.add_command('server', Server(threaded=True))
