09e70722fd10a9235176f276cde350711106bc4c
nalawadepiyush <nalawadepiyush@gmail.com>
2018-12-16 07:42:44 +0530

    HTML modified

--- slambook_restful/views/friend.py
+++ slambook_restful/views/friend.py
@@ -6,7 +6,7 @@ def save_new_friend(user, data):
     is_duplicate = Friend.check_if_duplicate_friend(user, data)
     is_current_user = Friend.check_if_current_user(user, data)
     if is_duplicate:
-        raise ResourceAlreadyPresent("Friend already exists with this email address")
+        raise ResourceAlreadyPresent("Your already have friend with this email address")
     if is_current_user:
         raise ResourceAlreadyPresent("Its your email id !! Dont Create your entry as your friend")
     Friend.add_friend_details(user, data)
@@ -15,5 +15,5 @@ def save_new_friend(user, data):
 def add_new_friend_remote(bearer, data):
     is_duplicate = Friend.check_if_duplicate_friend(bearer, data)
     if is_duplicate:
-        raise ResourceAlreadyPresent("Friend already exists with this email address")
+        raise ResourceAlreadyPresent("Your entry is already present at your friend !! ")
     Friend.add_friend_details(bearer, data)
